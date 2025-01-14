c8f8a3d100eab0f3c02242f44b3be80d8ee1441d
Dominik Kanjuh <dominik.kanjuh@outlook.com>
2025-01-14 03:57:59 +0100

    download script v2

--- database/download-sql.sh
+++ database/download-sql.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-# URLs for downloading
 AIR_POLLUTION_URL="https://1drv.ms/u/s!AqjaHlNJFPaTyPgcoE-xWs24SeERvQ?e=wxWQe6"
 EARTH_RELIEF_URL="https://1drv.ms/u/s!AqjaHlNJFPaTyPgbm1i6fFWasH1IFg?e=ZlQmtx"
 TEMPERATURE_URL="https://1drv.ms/u/s!AqjaHlNJFPaTyPga_Xvu1PGFiCOXkg?e=nVlDaU"
 
-# Download and move files
+TARGET_DIR="./database/sql"
+mkdir -p "$TARGET_DIR"
+
+touch "$TARGET_DIR/air_pollution_import.sql"
+touch "$TARGET_DIR/earth_relief_import.sql"
+touch "$TARGET_DIR/temperature_import.sql"
+
 curl -L "$AIR_POLLUTION_URL" -o /database/sql/air_pollution_import.sql
 curl -L "$EARTH_RELIEF_URL" -o /database/sql/earth_relief_import.sql
 curl -L "$TEMPERATURE_URL" -o /database/sql/temperature_import.sql
